7c2d8d6a2c7132f4e544be3b29cff44399951975
sai vinay <saiv71155@gmail.com>
2025-01-07 22:50:08 +0530

    set signup,signin,logout routes and jwt

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import router from './routes/auth.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
 dotenv.config();
@@ -9,7 +11,10 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Server is always ready');
 });
+app.use(express.json());
+app.use('/api/auth', router);
 
 app.listen(PORT, () => {
+    connectToMongoDB();
     console.log(`Server running on port ${PORT}`);
 });
