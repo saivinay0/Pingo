83646ae6bd9ce8948e16568f44f41fe513420237
sai vinay <saiv71155@gmail.com>
2025-01-09 18:49:27 +0530

    Implemented pages and components

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,44 @@
+import { create } from 'zustand';
+import toast from 'react-hot-toast';
+import { axiosInstance } from '../lib/axios';
+
+
+export const useChatStore = create((set) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+
+
+
+    getusers: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const { data } = await axiosInstance.get('/messages/users');
+            set({ users: data });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+        finally {
+            set({ isUsersLoading: false });
+        }
+    },
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true });
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: res.data });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+        finally {
+            set({ isMessagesLoading: false });
+        }
+    },
+    setSelectedUser: (selectedUser) => {
+        set({ selectedUser });
+    },
+
+
+}));
